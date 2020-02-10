27c193efdba7bf5d5a26b24a50a2a3e0a51a9b2f
arjit-webonise <arjit.acharya@weboniselab.com>
2020-02-10 12:25:30 +0530

    convert canvas into data url and preview

--- image-pointer/src/app/app.component.ts
+++ image-pointer/src/app/app.component.ts
@@ -6,27 +6,27 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit {
-  constructor(){
+  constructor() {
 
   }
-  @ViewChild('canvasEl', {static: false}) canvasEl: ElementRef;
-  
+  @ViewChild('canvasEl', { static: false }) canvasEl: ElementRef;
+
   /** Canvas 2d context */
   private context: CanvasRenderingContext2D;
-
+  savedImage = ''
   title = 'image-pointer';
   imageBasePath = '../assets/serverImages/'
   imgUrl = ''
-  imageCollection = ['hand.jpeg','jaw.jpeg','knee.jpeg']
+  imageCollection = ['hand.jpeg', 'jaw.jpeg', 'knee.jpeg']
   imageObj = null;
   rect = {
-    w:0,
-    h:0,
-    startX:0,
-    startY:0
+    w: 0,
+    h: 0,
+    startX: 0,
+    startY: 0
   };
   drag = false
-  
+
   getImg(image: string) {
     return `${this.imageBasePath}${image}`;
   }
@@ -34,50 +34,56 @@ export class AppComponent implements AfterViewInit {
   onImgSelect = (e) => {
     this.context.clearRect(0, 0, 500, 500);
     this.imageObj = new Image();
-    this.imageObj.onload =  () => { 
-       this.context.drawImage(this.imageObj, 0, 0); 
-     };
-    this.imageObj.src= e.target.src;    
+    this.imageObj.onload = () => {
+      this.context.drawImage(this.imageObj, 0, 0);
+    };
+    this.imageObj.src = e.target.src;
   }
 
-    
+
   ngAfterViewInit() {
-    var self=this;    
-    
+    var self = this;
+
     self.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
     self.imageObj = new Image();
-    self.imageObj.onload =  () => { 
-       self.context.drawImage(self.imageObj, 0, 0); 
-     };
-    self.imageObj.src = '../assets/serverImages/hand.jpeg';    
+    self.imageObj.onload = () => {
+      self.context.drawImage(self.imageObj, 0, 0);
+    };
+    self.imageObj.src = '../assets/serverImages/hand.jpeg';
     self.canvasEl.nativeElement.addEventListener('mousedown', self.mouseDown, false);
     self.canvasEl.nativeElement.addEventListener('mouseup', self.mouseUp, false);
     self.canvasEl.nativeElement.addEventListener('mousemove', self.mouseMove, false);
-     
+
   }
-  draw = () =>  {
+  draw = () => {
     this.context.strokeRect(this.rect.startX, this.rect.startY, this.rect.w, this.rect.h);
-	}
-  
-  mouseDown = (e) => { 
-	  this.rect.startX = e.pageX - e.target.offsetLeft;
-	  this.rect.startY = e.pageY - e.target.offsetTop;
-	  this.drag = true;
   }
-  
-  mouseUp = () => { 
+
+  mouseDown = (e) => {
+    this.rect.startX = e.pageX - e.target.offsetLeft;
+    this.rect.startY = e.pageY - e.target.offsetTop;
+    this.drag = true;
+  }
+
+  mouseUp = () => {
     this.drag = false;
   }
 
-  mouseMove = (e) => {    
+  mouseMove = (e) => {
     if (this.drag) {
-        this.context.clearRect(0, 0, 500, 500);
-        this.context.drawImage(this.imageObj, 0, 0);
-        this.rect.w = (e.pageX - e.target.offsetLeft) - this.rect.startX;
-        this.rect.h = (e.pageY - e.target.offsetTop) - this.rect.startY;
-        this.context.strokeStyle = 'red';
-        this.context.strokeRect(this.rect.startX, this.rect.startY, this.rect.w, this.rect.h);
-        this.draw();
+      this.context.clearRect(0, 0, 500, 500);
+      this.context.drawImage(this.imageObj, 0, 0);
+      this.rect.w = (e.pageX - e.target.offsetLeft) - this.rect.startX;
+      this.rect.h = (e.pageY - e.target.offsetTop) - this.rect.startY;
+      this.context.strokeStyle = 'red';
+      this.context.strokeRect(this.rect.startX, this.rect.startY, this.rect.w, this.rect.h);
+      this.draw();
     }
-} 
+  }
+
+  upload = (e) => {
+    let canvas = <HTMLCanvasElement>document.getElementById('canvas');
+    this.savedImage = canvas.toDataURL();
+  }
+
 }
